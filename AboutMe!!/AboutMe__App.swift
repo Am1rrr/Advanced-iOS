79748c35e3906ba9a0f3d5e75194f76dbacdae9e
Am1rrr <hamraev639@gmail.com>
2025-02-14 21:59:15 +0500

    Initial Commit

---
+++ AboutMe!!/AboutMe__App.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMe__App.swift
+//  AboutMe!!
+//
+//  Created by amir on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AboutMe__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
